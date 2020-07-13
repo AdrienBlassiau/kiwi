6a7827e96e44cb337eb327bd3408d99bcba796cd
AdrienBlassiau <adblassiau@gmail.com>
2020-07-13 18:40:57 +0200

    feat(scrollbr): add a custom scrollbar

--- app/renderer/css/AppCss.js
+++ app/renderer/css/AppCss.js
@@ -37,6 +37,19 @@ const Style = createGlobalStyle`
 	--imageBorderRadius: 8px;
 }
 
+::-webkit-scrollbar {
+  width: 10px;
+}
+
+// ::-webkit-scrollbar-track {
+//   background: #f1f1f1;
+// }
+
+::-webkit-scrollbar-thumb {
+  background: var(--dark-white-color);
+}
+
+
 body {
 	font-family: 'Source Sans Pro';
 	background-color: var(--dark-color);
@@ -969,6 +982,49 @@ a:link:active, a:visited:active {
 .active-color {
   background-color: white;
 }
+
+.main-bottom-container{
+	display: flex;
+	width: 100%;
+	background-color: var(--dark-white-color);
+	color: white;
+	padding: 0;
+	line-height: 30px;
+	justify-content: left;
+	overflow: visible;
+	position: absolute;
+	bottom: 0;
+	left: 0;
+	right: 0;
+	box-sizing: border-box;
+	width: 100%;
+	font-size: 13px;
+	padding: 0 16px;
+	overflow: hidden;
+	flex-shrink: 0;
+	align-items: center;
+	justify-content: center;
+	user-select: none;
+	zoom: 1;
+	line-height: 22px;
+	height: 30px;
+	display: flex;
+	z-index: 99999;
+	-webkit-box-shadow: 0px -7px 10px -11px rgba(0,0,0,0.75);
+  	-moz-box-shadow: 0px -7px 10px -11px rgba(0,0,0,0.75);
+  	box-shadow: 0px -7px 10px -11px rgba(0,0,0,0.75);
+}
+
+.main-bottom-data{
+	display: flex;
+	justify-content: space-between;
+	width: 100%;
+}
+
+.site-name {
+	color:white;
+}
+s
 `;
 
 export default Style;
